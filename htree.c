2e990bc8f344f1297e05dd2f39118ce2d44eb14c
Bradley Mixon <99378881+bradleymixon@users.noreply.github.com>
2023-04-15 00:43:06 -0500

    Update htree.c

--- htree.c
+++ htree.c
@@ -1,120 +1,160 @@
-#include <stdio.h>     
-#include <stdlib.h>   
-#include <stdint.h>  
-#include <inttypes.h>  
-#include <errno.h>     // for EINTR
-#include <fcntl.h>     
-#include <unistd.h>    
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <stdint.h>
+
+#include <inttypes.h>
+
+#include <errno.h> // for EINTR
+
+#include <fcntl.h>
+
+#include <unistd.h>
+
 #include <sys/types.h>
+
 #include <sys/stat.h>
 #include <pthread.h>
-#include <sys/time.h>
-#include <assert.h>
-
+#include <string.h>
+#include <sys/time.h> //for GetTime()
+#include <assert.h> //for GetTime()
 
 // Print out the usage of the program and exit.
+
 void Usage(char*);
+
 uint32_t jenkins_one_at_a_time_hash(const uint8_t* , uint64_t );
-void *tree(void *arg);
+void *thread_function(void *args);
 double GetTime();
 
+typedef struct {
+    int32_t fd;
+    uint32_t start_block;
+    uint32_t blocks_per_thread;
+    uint32_t thread_number;
+} ThreadArgs;
+
 // block size
+
 #define BSIZE 4096
 
-int 
-main(int argc, char** argv) 
+int
+
+main(int argc, char** argv)
+
 {
-  int32_t fd;
-  uint32_t nblocks;
-  int ht;
 
-  // input checking 
-  if (argc != 3)
-    Usage(argv[0]);
+int32_t fd;
 
-  // open input file
-  fd = open(argv[1], O_RDWR);
-  if (fd == -1) {
-    perror("open failed");
-    exit(EXIT_FAILURE);
-  }
-  // use fstat to get file size
-  struct stat sb;
-  if(fstat(fd, &sb) == -1){ //filesize in sb.st_size
+uint32_t nblocks;
+
+// input checking
+
+if (argc != 3)
+
+Usage(argv[0]);
+
+// open input file
+
+fd = open(argv[1], O_RDWR);
+
+if (fd == -1) {
+
+perror("open failed");
+
+exit(EXIT_FAILURE);
+
+}
+
+// use fstat to get file size
+struct stat sb;
+  if(fstat(fd, &sb) == -1){
     perror("fstat error");
     exit(EXIT_FAILURE);
   }
-  // calculate nblocks 
-  nblocks = sb.st_size / BSIZE; //num of blocks = file size divided by size of each block
+  //filesize in sb.st_size
 
-  printf(" no. of blocks = %u \n", nblocks);
+// calculate nblocks
+nblocks = sb.st_size / BSIZE;
 
-  double start = GetTime();
+printf(" no. of blocks = %u \n", nblocks);
 
-  // calculate hash value of the input file
-  ht = atoi(argv[2]);
-  pthread_t p1;
-	pthread_create(&p1, NULL, tree, &ht);
-	pthread_join(p1, NULL);
+double start = GetTime();
 
+// calculate hash value of the input file
+uint32_t num_threads = atoi(argv[2]);
+pthread_t threads[num_threads];
+ThreadArgs thread_args[num_threads];
 
-  double end = GetTime();
-  //printf("hash value = %u \n", hash);
-  printf("time taken = %f \n", (end - start));
-  close(fd);
-  return EXIT_SUCCESS;
+for (uint32_t i = 0; i < num_threads; i++) {
+    thread_args[i].fd = fd;
+    thread_args[i].start_block = i * (nblocks / num_threads);
+    thread_args[i].blocks_per_thread = nblocks / num_threads;
+    thread_args[i].thread_number = i;
+    pthread_create(&threads[i], NULL, thread_function, (void *)&thread_args[i]);
 }
 
-uint32_t 
-jenkins_one_at_a_time_hash(const uint8_t* key, uint64_t length) 
-{
-  uint64_t i = 0;
-  uint32_t hash = 0;
+uint32_t final_hash = 0;
+uint32_t thread_hash;
+for (uint32_t i = 0; i < num_threads; i++) {
+    pthread_join(threads[i], (void **)&thread_hash);
+    final_hash ^= thread_hash;
+}
+printf("hash value = %u \n", final_hash);
 
-  while (i != length) {
-    hash += key[i++];
-    hash += hash << 10;
-    hash ^= hash >> 6;
-  }
-  hash += hash << 3;
-  hash ^= hash >> 11;
-  hash += hash << 15;
-  return hash;
+double end = GetTime();
+
+//printf("hash value = %u \n", hash);
+
+printf("time taken = %f \n", (end - start));
+
+close(fd);
+
+return EXIT_SUCCESS;
+
 }
 
+uint32_t
 
-void 
-Usage(char* s) 
+jenkins_one_at_a_time_hash(const uint8_t* key, uint64_t length)
+
 {
-  fprintf(stderr, "Usage: %s filename num_threads \n", s);
-  exit(EXIT_FAILURE);
+
+uint64_t i = 0;
+
+uint32_t hash = 0;
+
+while (i != length) {
+
+hash += key[i++];
+
+hash += hash << 10;
+
+hash ^= hash >> 6;
+
 }
 
-void* 
-tree(void* arg) 
+hash += hash << 3;
+
+hash ^= hash >> 11;
+
+hash += hash << 15;
+
+return hash;
+
+}
+
+void
+
+Usage(char* s)
+
 {
-	int currHt = *(int*)arg; //find height of current thread
-	if(currHt == -1) //Base case of recursion, when height is -1, stop creating new threads
-		return NULL;
 
-	if(currHt == 0) //Check if current thread is an internal or leaf node of the binary tree
-		printf("Leaf Thread at height %d\n", currHt);
-	else
-		printf("Int. Thread at height %d\n", currHt);
+fprintf(stderr, "Usage: %s filename num_threads \n", s);
 
-	pthread_t LChild;
-	pthread_t RChild;
-	int childHT = currHt - 1; //calculate height of the children of the current thread, height of left and right will be the same
+exit(EXIT_FAILURE);
 
-	//Create and join thread for left child of current thread
-	pthread_create(&LChild, NULL, tree, &childHT);
-	pthread_join(LChild, NULL);
-
-	//Create and join thread for right child of current thread
-	pthread_create(&RChild, NULL, tree, &childHT);
-	pthread_join(RChild, NULL);
-
-    return NULL;
 }
 
 double GetTime() {
@@ -122,4 +162,18 @@ double GetTime() {
     int rc = gettimeofday(&t, NULL);
     assert(rc == 0);
     return (double)t.tv_sec + (double)t.tv_usec/1e6;
+}
+
+
+void *thread_function(void *args) {
+    ThreadArgs *ta = (ThreadArgs *)args;
+    uint8_t buffer[BSIZE];
+    uint32_t block_hash;
+    uint64_t hash = 0;
+    for (uint32_t i = 0; i < ta->blocks_per_thread; i++) {
+        pread(ta->fd, buffer, BSIZE, (ta->start_block + i) * BSIZE);
+        block_hash = jenkins_one_at_a_time_hash(buffer, BSIZE);
+        hash ^= block_hash;
+    }
+    pthread_exit((void *)(uintptr_t)hash);
 }
